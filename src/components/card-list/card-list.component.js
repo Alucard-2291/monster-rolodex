372f4348cfb2e844f4f711fc2ea56013b03b7b7c
Alucard-2291 <saurabhs1304@gmail.com>
2022-10-19 15:04:01 +0530

    monster rolodex-1

---
+++ src/components/card-list/card-list.component.js
@@ -0,0 +1,21 @@
+import { Component } from 'react';
+import CardContainer from '../card/card.compnent';
+import './card-list.style.css';
+
+class CardList extends Component {
+  render() {
+    const { monsters } = this.props;
+
+    return (
+      <div className="card-list">
+        {monsters.map((monster) => {
+          return (
+            <CardContainer monster = {monster} key={monster.id}/>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default CardList;
